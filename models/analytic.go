36769a8956cfecadaf5c699e57dcdfd943a7b1bd
ponlv <levanpon1009@gmail.com>
2023-05-12 14:01:57 +0700

    update

--- models/analytic.go
+++ models/analytic.go
@@ -4,6 +4,10 @@ type ArgumentsInput struct {
 	Input int `json:"Input"`
 }
 
+type TransferObjectsInput struct {
+	Input int `json:"Input"`
+}
+
 type TransactionTypeMoveCall struct {
 	Package   string        `json:"package"`
 	Module    string        `json:"module"`
